fd3945f09a58298166b58803c4170abe6076461d
Justin Smith <phreenet@gmail.com>
2014-09-15 21:12:21 -0400

    testing rebase

---
+++ WikiHere/TheViewController.m
@@ -0,0 +1,49 @@
+//
+//  TheViewController.m
+//  WikiHere
+//
+//  Created by Justin Smith on 9/15/14.
+//  Copyright (c) 2014 CMSC 495. All rights reserved.
+//
+
+#import "TheViewController.h"
+
+@interface TheViewController ()
+
+@end
+
+@implementation TheViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
