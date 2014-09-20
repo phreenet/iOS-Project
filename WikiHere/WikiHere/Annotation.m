3f31523b3e947b78a3a6ef6f8d5bc91bb8a02d47
Justin Smith <phreenet@gmail.com>
2014-09-20 16:00:53 -0400

    Created Annotation Class

---
+++ WikiHere/WikiHere/Annotation.m
@@ -0,0 +1,14 @@
+//
+//  Annotation.m
+//  WikiHere
+//
+//  Created by Justin Smith on 9/20/14.
+//  Copyright (c) 2014 CMSC 495. All rights reserved.
+//
+
+#import "Annotation.h"
+
+@implementation Annotation
+@synthesize coordinate, title, subtitle;
+
+@end
