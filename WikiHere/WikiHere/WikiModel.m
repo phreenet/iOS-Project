306064985b06129111827fe4a80eb6a555350346
jk <Tank@Tanks-MacBook-Pro.local>
2014-09-27 12:31:52 -0400

    added new WikiModel files - small commits baby, small commits

---
+++ WikiHere/WikiHere/WikiModel.m
@@ -0,0 +1,13 @@
+//
+//  WikiModel.m
+//  WikiHere
+//
+//  Created by Jeremy on 9/27/14.
+//  Copyright (c) 2014 CMSC 495. All rights reserved.
+//
+
+#import "WikiModel.h"
+
+@implementation WikiModel
+
+@end
