e741ed399267ac1fa6cffc72debce0c18e6303cd
Justin Smith <phreenet@gmail.com>
2014-09-30 15:07:15 -0400

    Updating CocoaPods for Xcode 6 and removing uneeded Pods

--- WikiHere/Pods/Headers/SHXMLParser/SHXMLParser.h
+++
@@ -1,1 +0,0 @@
-../../SHXMLParser/SHXMLParser/SHXMLParser.h
---
+++ WikiHere/Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// SHXMLParser
+#define COCOAPODS_POD_AVAILABLE_SHXMLParser
+#define COCOAPODS_VERSION_MAJOR_SHXMLParser 1
+#define COCOAPODS_VERSION_MINOR_SHXMLParser 1
+#define COCOAPODS_VERSION_PATCH_SHXMLParser 0
+
+// WYPopoverController
+#define COCOAPODS_POD_AVAILABLE_WYPopoverController
+#define COCOAPODS_VERSION_MAJOR_WYPopoverController 0
+#define COCOAPODS_VERSION_MINOR_WYPopoverController 2
+#define COCOAPODS_VERSION_PATCH_WYPopoverController 2
+
